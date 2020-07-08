42beabecbc0b1b57ac650cde8ea142b732274210
jojanaho <janne.ojanaho@iki.fi>
2020-07-08 23:52:51 +0300

    Added additional test for multiple H1 headers

--- packages/foam-workspace-manager/test/utils/utils.test.ts
+++ packages/foam-workspace-manager/test/utils/utils.test.ts
@@ -20,15 +20,26 @@ foo
 bar
 `;
 
+const pageD = `
+# Page D
+hello world
+
+# Another header
+hello world
+`;
+
 describe('WorkspaceManager', () => {
   it('finds top level headings', () => {
     const titleA = parseNoteTitleFromMarkdown(pageA);
     const titleB = parseNoteTitleFromMarkdown(pageB);
     const titleC = parseNoteTitleFromMarkdown(pageC);
+    const titleD = parseNoteTitleFromMarkdown(pageD);
 
     expect(titleA).toEqual('Page A');
     expect(titleB).toEqual('Page B');
     expect(titleC).toBeNull();
+    // in case of multiple top level headings, the first one rules
+    expect(titleD).toEqual('Page D');
   });
 
   it('finds wikilinks', () => {

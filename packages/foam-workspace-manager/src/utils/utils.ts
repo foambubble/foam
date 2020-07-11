f2efd141862fc83e03255fd3cac33f9a46087782
Janne Ojanaho <janne.ojanaho@iki.fi>
2020-07-11 09:59:05 +0300

    Changed to unifiedjs-based markdown parsing (#78)

--- packages/foam-workspace-manager/src/utils/utils.ts
+++ packages/foam-workspace-manager/src/utils/utils.ts
@@ -1,23 +1,45 @@
-// @todo convert this to use ast parsing
-
-// import unified from 'unified';
-// import markdown from 'remark-parse';
-// import wikiLinkPlugin from 'remark-wiki-link';
-// let processor = unified()
-//   .use(markdown, { gfm: true })
-//   .use(wikiLinkPlugin);
-
-import { findTopLevelHeading, findWikilinksInMarkdown } from './markdown-utils';
+import unified from 'unified';
+import markdownParse from 'remark-parse';
+import wikiLinkPlugin from 'remark-wiki-link';
+import visit from 'unist-util-visit';
+import { Node, Parent } from 'unist';
 
 // @ts-expect-error
 export function readWorkspaceFile(filename: string): string {
   throw new Error('Not implemented');
 }
 
+// pipeline cache
+let processor: unified.Processor | null = null;
+
+function parse(markdown: string): Node {
+  processor =
+    processor ||
+    unified()
+      .use(markdownParse, { gfm: true })
+      .use(wikiLinkPlugin);
+  return processor.parse(markdown);
+}
+
 export function parseNoteTitleFromMarkdown(markdown: string): string | null {
-  return findTopLevelHeading(markdown);
+  let title: string | null = null;
+  const tree = parse(markdown);
+  visit(tree, node => {
+    if (node.type === 'heading' && node.depth === 1) {
+      title = ((node as Parent)!.children[0].value as string) || null;
+    }
+    return title === null;
+  });
+  return title;
 }
 
 export function parseNoteLinksFromMarkdown(markdown: string): string[] {
-  return findWikilinksInMarkdown(markdown);
+  let links: string[] = [];
+  const tree = parse(markdown);
+  visit(tree, node => {
+    if (node.type === 'wikiLink') {
+      links.push(node.value as string);
+    }
+  });
+  return links;
 }

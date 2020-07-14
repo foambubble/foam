6bd9aaa9493175729c764a1daeef3d031af48acd
Jani Eväkallio <jani.evakallio@gmail.com>
2020-07-14 10:05:47 +0100

    Export stringifyMarkdownLinkReferenceDefinition from foam-core

--- packages/foam-core/src/index.ts
+++ packages/foam-core/src/index.ts
@@ -3,20 +3,21 @@ import { NoteGraph, Note, NoteLink } from './note-graph';
 export {
   createNoteFromMarkdown,
   createMarkdownReferences,
+  stringifyMarkdownLinkReferenceDefinition,
 } from './markdown-provider';
 
-export { NoteGraph, Note, NoteLink }
+export { NoteGraph, Note, NoteLink };
 
 export interface FoamConfig {
   // TODO
 }
 
 export interface Foam {
-  notes: NoteGraph
+  notes: NoteGraph;
   // config: FoamConfig
 }
 
 export const createFoam = (config: FoamConfig) => ({
   notes: new NoteGraph(),
   config: config,
-})
+});

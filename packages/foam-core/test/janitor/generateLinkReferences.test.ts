6fa858f8d4116ffa7f1e9f38642fe555e65c4f48
chirag-singhal <csinghal208@gmail.com>
2020-07-14 16:17:56 +0530

    Add remove link definitions test to generateLinkReferences janitor method

--- packages/foam-core/test/janitor/generateLinkReferences.test.ts
+++ packages/foam-core/test/janitor/generateLinkReferences.test.ts
@@ -41,7 +41,32 @@ describe('generateLinkReferences', () => {
     expect(actual!.newText).toEqual(expected.newText);
   });
 
-  it('should remove link definitions from a file that has them, if no links are present', () => {});
+  it('should remove link definitions from a file that has them, if no links are present', () => {
+    const note = _graph.getNote('second-document') as Note;
+    
+    const expected = {
+      newText: "",
+      range: {
+        start: {
+          line: 7,
+          column: 1,
+          offset: 105,
+        },
+        end: {
+          line: 9,
+          column: 43,
+          offset: 269,
+        },
+      },
+    };
+    
+    const actual = generateLinkReferences(note!, _graph);
+
+    expect(actual!.range.start).toEqual(expected.range.start);
+    expect(actual!.range.end).toEqual(expected.range.end);
+    expect(actual!.newText).toEqual(expected.newText);
+
+  });
 
   it('should update link definitions if they are present but changed', () => {});
 

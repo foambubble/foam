bb8d0dabba12022ddfdfbd9ae8c7dad2dffa3aa6
chirag-singhal <csinghal208@gmail.com>
2020-07-14 20:44:54 +0530

    added tests for generateHeading in janitor

---
+++ packages/foam-core/test/janitor/generateHeadings.test.ts
@@ -0,0 +1,52 @@
+import { NoteGraph, Note } from '../../src/note-graph';
+import { generateHeading } from '../../src/janitor';
+import { scaffold } from '../__scaffold__';
+
+describe('generateHeadings', () => {
+  let _graph: NoteGraph;
+
+  beforeAll(async () => {
+    _graph = await scaffold();
+  });
+
+  it('should add heading to a file that does not have them', () => {
+    const note = _graph.getNote('file-without-title') as Note;
+
+    const expected = {
+      newText: `# File without Title
+
+`,
+      range: {
+        start: {
+          line: 0,
+          column: 0,
+          offset: 0,
+        },
+        end: {
+          line: 0,
+          column: 0,
+          offset: 0,
+        },
+      },
+    }
+
+    const actual = generateHeading(note!);
+
+    expect(actual!.range.start).toEqual(expected.range.start);
+    expect(actual!.range.end).toEqual(expected.range.end);
+    expect(actual!.newText).toEqual(expected.newText);
+
+  });
+
+  it('should not cause any changes to a file that does heading', () => {
+    const note = _graph.getNote('index') as Note;
+
+    const expected = null;
+
+    const actual = generateHeading(note!);
+
+    expect(actual).toEqual(expected);
+
+  })
+
+});

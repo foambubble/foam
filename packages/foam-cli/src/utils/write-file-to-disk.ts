626f64aec05bdcdd33846e08e1b28711d4fe1043
Ankit Tiwari <ankitt255@gmail.com>
2020-07-16 13:32:30 +0530

    Propagate the error from writeFileToDisk method

--- packages/foam-cli/src/utils/write-file-to-disk.ts
+++ packages/foam-cli/src/utils/write-file-to-disk.ts
@@ -1,8 +1,5 @@
 import * as fs from 'fs';
 
-export const writeFileToDisk = async (fileUri: string, data: string): Promise<Boolean> => {
-  return fs.promises.writeFile(fileUri, data).then(() => true).catch(err => {
-    console.log('error while writing to file: ', err)
-    return false;
-  })
+export const writeFileToDisk = async (fileUri: string, data: string) => {
+  return fs.promises.writeFile(fileUri, data);
 }

2817fc659ba987ca533808a67f14b663ec12ea7f
Ankit Tiwari <ankitt255@gmail.com>
2020-07-24 14:40:31 +0530

    WIP: Add janitor command in foam-vscode

--- packages/foam-vscode/src/features/index.ts
+++ packages/foam-vscode/src/features/index.ts
@@ -1,5 +1,6 @@
 import createReferences from "./wikilink-reference-generation";
 import openDailyNote from "./open-daily-note";
+import janitor from "./janitor";
 import { FoamFeature } from "../types";
 
-export const features: FoamFeature[] = [createReferences, openDailyNote];
+export const features: FoamFeature[] = [createReferences, openDailyNote, janitor];

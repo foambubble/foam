b42fa7f3ae2cb1a9e4a5090094006e577b9fac5e
chirag-singhal <csinghal208@gmail.com>
2020-07-25 02:45:07 +0530

    fixed multi unsaved files text edits

--- packages/foam-vscode/src/features/janitor.ts
+++ packages/foam-vscode/src/features/janitor.ts
@@ -24,24 +24,21 @@ import {
 
 const feature: FoamFeature = {
   activate: async (context: ExtensionContext, foamPromise: Promise<Foam>) => {
-    const foam = await foamPromise;
     context.subscriptions.push(
-      commands.registerCommand("foam-vscode.janitor", () => janitor(foam))
+      commands.registerCommand("foam-vscode.janitor", () => janitor())
     );
   },
 };
 
-async function janitor(foam: Foam) {
-  const foamNew = await bootstrap(getConfig()); 
-  const notes = foamNew.notes.getNotes().filter(Boolean);
+async function janitor() {
+  const foam = await bootstrap(getConfig()); 
+  const notes = foam.notes.getNotes().filter(Boolean);
 
-  const dirtyEditors = window.visibleTextEditors.filter(editor => editor.document.isDirty);
-  const dirtyEditorsFileName = dirtyEditors.map(editor => editor.document.fileName);
+  const dirtyTextDocuments = workspace.textDocuments.filter(textDocument => (textDocument.languageId === 'markdown' || textDocument.languageId === 'mdx') && textDocument.isDirty);
+  const dirtyEditorsFileName = dirtyTextDocuments.map(dirtyTextDocument => dirtyTextDocument.fileName);
 
   const dirtyNotes: Note[] = notes.filter(note => dirtyEditorsFileName.includes(note.path));
   const nonDirtyNotes: Note[] = notes.filter(note => !dirtyEditorsFileName.includes(note.path));
-
-  console.log(nonDirtyNotes, dirtyNotes);
 
   // Apply Text Edits to Non Dirty Notes using fs module just like CLI
 
@@ -63,37 +60,32 @@ async function janitor(foam: Foam) {
 
   // Handle dirty editors 
 
+  dirtyTextDocuments.reduce((accDirtyTextDocument, nextTextDocument) => {
+    return accDirtyTextDocument.then(() => {
+      return window.showTextDocument(nextTextDocument).then(editor => {
+        const note = dirtyNotes.find(n => n.path === editor.document.fileName);
 
-   dirtyEditors.map(async editor => {
-     let text = editor.document.getText(); // This includes unsaved changes as well
+        // Get edits
+        const heading = generateHeading(note);
+        let definitions = generateLinkReferences(note, foam.notes);
 
-     const note = dirtyNotes.find(n => n.path === editor.document.fileName);
-
-     // Get edits
-     const heading = generateHeading(note);
-     let definitions = generateLinkReferences(note, foam.notes);
-
-     console.log(heading);
-     // apply Edits
-     editor.edit(editBuilder => {
-       if(heading) {
-        const start = new Position(heading.range.start.line, heading.range.start.column);
-        console.log(start, heading.newText);
-        editBuilder.insert(start, heading.newText);
-      }
-      if(definitions) {
-        const start = new Position(definitions.range.start.line - 1, definitions.range.start.column);
-        const end = new Position(definitions.range.end.line - 1, definitions.range.end.column);
-        const range = new Range(start, end);
-        editBuilder.replace(range, definitions!.newText);
-      }
-     });
-
-     // Use editor.edit API to apply the text edit
-
-     
-
-    });
+        // apply Edits
+          return editor.edit(editBuilder => {
+            if(heading) {
+              const start = new Position(heading.range.start.line, heading.range.start.column);
+              editBuilder.insert(start, heading.newText);
+            }
+            if(definitions) {
+              const start = new Position(definitions.range.start.line - 1, definitions.range.start.column);
+              const end = new Position(definitions.range.end.line - 1, definitions.range.end.column);
+              const range = new Range(start, end);
+              editBuilder.replace(range, definitions!.newText);
+            }
+          });
+        });
+      });
+    }, Promise.resolve()
+  );
 }
 
 

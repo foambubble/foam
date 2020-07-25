dbb87ee4835a67c260ef4acc2a623f0332fdb105
Jani Eväkallio <jani.evakallio@gmail.com>
2020-07-25 10:14:34 +0100

    Add notes to NoteGraph when they are created

--- packages/foam-vscode/src/extension.ts
+++ packages/foam-vscode/src/extension.ts
@@ -5,10 +5,25 @@
 "use strict";
 
 import * as fs from "fs";
-import { workspace, ExtensionContext, window, EndOfLine } from "vscode";
+import {
+  workspace,
+  ExtensionContext,
+  window,
+  EndOfLine,
+  Uri,
+  FileSystemWatcher
+} from "vscode";
 
-import { createNoteFromMarkdown, createFoam, FoamConfig } from "foam-core";
+import {
+  createNoteFromMarkdown,
+  createFoam,
+  FoamConfig,
+  Foam
+} from "foam-core";
+
 import { features } from "./features";
+
+let workspaceWatcher: FileSystemWatcher | null = null;
 
 export function activate(context: ExtensionContext) {
   const foamPromise = bootstrap(getConfig());
@@ -17,20 +32,52 @@ export function activate(context: ExtensionContext) {
   });
 }
 
+export function deactivate() {
+  workspaceWatcher?.dispose();
+}
+
+function isLocalMarkdownFile(uri: Uri) {
+  return uri.scheme === "file" && uri.path.match(/\.(md|mdx|markdown)/i);
+}
+
+async function registerFile(foam: Foam, localUri: Uri) {
+  // read file from disk (async)
+  const path = localUri.fsPath;
+  const data = await fs.promises.readFile(path);
+  const markdown = (data || "").toString();
+
+  // create note
+  const eol =
+    window.activeTextEditor?.document?.eol === EndOfLine.CRLF ? "\r\n" : "\n";
+  const note = createNoteFromMarkdown(path, markdown, eol);
+
+  // add to graph
+  foam.notes.setNote(note);
+  return note;
+}
+
 const bootstrap = async (config: FoamConfig) => {
   const files = await workspace.findFiles("**/*");
   const foam = createFoam(config);
-  await Promise.all(
-    files
-      .filter(f => f.scheme === "file" && f.path.match(/\.(md|mdx|markdown)/i))
-      .map(f => {
-        return fs.promises.readFile(f.fsPath).then(data => {
-          const markdown = (data || "").toString();
-          const eol = window.activeTextEditor?.document?.eol === EndOfLine.CRLF ? "\r\n" : "\n";
-          foam.notes.setNote(createNoteFromMarkdown(f.fsPath, markdown, eol));
-        });
-      })
+  const addFile = (uri: Uri) => registerFile(foam, uri);
+
+  await Promise.all(files.filter(isLocalMarkdownFile).map(addFile));
+
+  workspaceWatcher = workspace.createFileSystemWatcher(
+    "**/*",
+    false,
+    true,
+    true
   );
+  
+  workspaceWatcher.onDidCreate(uri => {
+    if (isLocalMarkdownFile(uri)) {
+      addFile(uri).then(() => {
+        console.log(`Added ${uri} to workspace`);
+      });
+    }
+  });
+
   return foam;
 };
 

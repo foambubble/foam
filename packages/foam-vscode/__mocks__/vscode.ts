fd2f00ec468ae52570f02707358e61cec0177c4f
Joe Previte <jprevite@fb.com>
2020-08-02 12:28:37 -0700

    feat: add vscode mock

---
+++ packages/foam-vscode/__mocks__/vscode.ts
@@ -0,0 +1,16 @@
+/*
+Note: this is needed in order to test certain parts
+of functionality of `foam-vscode`
+
+Following the advice from this article:
+https://www.richardkotze.com/coding/unit-test-mock-vs-code-extension-api-jest
+
+combined with advice from this GitHub issue comment:
+https://github.com/microsoft/vscode-test/issues/37#issuecomment-584744386
+*/
+
+const vscode = {
+    // Add values and methods as needed for tests
+};
+
+module.exports = vscode;

6ecb45eb7351a4c025e9ba344f15e94ad111a701
Jonathan Ingall <jonny@mondago.com>
2020-10-24 13:44:14 +0100

    Refactor changes

--- packages/foam-vscode/src/features/open-dated-note.ts
+++ packages/foam-vscode/src/features/open-dated-note.ts
@@ -1,3 +1,4 @@
+import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from "constants";
 import {
   workspace,
   ExtensionContext,
@@ -20,18 +21,18 @@ interface DateSnippet {
 }
 
 const foamConfig = workspace.getConfiguration("foam");
+const foamExtension = foamConfig.get("openDailyNote.fileExtension");
+const foamLinkReferenceDefinitions = foamConfig.get(
+  "edit.linkReferenceDefinitions"
+);
 
-const getDailyNoteLink = (
-  date: Date,
-  extension: string,
-  linkReferenceDefinitions: LinkReferenceDefinitionsSetting
-) => {
+const getDailyNoteLink = (date: Date) => {
   let name = getDailyNoteFileName(foamConfig, date);
   if (
-    linkReferenceDefinitions ===
+    foamLinkReferenceDefinitions ===
     LinkReferenceDefinitionsSetting.withoutExtensions
   ) {
-    name = name.replace(`.${extension}`, "");
+    name = name.replace(`.${foamExtension}`, "");
   }
   return `[[${name}]]`;
 };
@@ -40,6 +41,11 @@ const snippets: (() => DateSnippet)[] = [
   () => ({
     detail: "Insert a link to today's daily note",
     snippet: "/day",
+    date: new Date()
+  }),
+  () => ({
+    detail: "Insert a link to today's daily note",
+    snippet: "/today",
     date: new Date()
   }),
   () => {
@@ -52,11 +58,6 @@ const snippets: (() => DateSnippet)[] = [
   }
 ];
 
-/**
- * I think to start working with this you should take a look at emmet
- * https://github.com/Microsoft/vscode/tree/master/extensions/emmet
- * Also, snippet string: https://code.visualstudio.com/api/references/vscode-api#SnippetString
- */
 const computedSnippets: ((number: number) => DateSnippet)[] = [
   (days: number) => {
     const today = new Date();
@@ -82,6 +83,18 @@ const computedSnippets: ((number: number) => DateSnippet)[] = [
       )
     };
   },
+  (months: number) => {
+    const today = new Date();
+    return {
+      detail: `Insert a date ${months} month(s) from now`,
+      snippet: `/+${months}m`,
+      date: new Date(
+        today.getFullYear(),
+        today.getMonth() + months,
+        today.getDate()
+      )
+    };
+  },
   (years: number) => {
     const today = new Date();
     return {
@@ -104,11 +117,8 @@ const completions: CompletionItemProvider = {
         snippet,
         CompletionItemKind.Snippet
       );
-      completionItem.insertText = getDailyNoteLink(
-        date,
-        foamConfig.get("openDailyNote.fileExtension"),
-        foamConfig.get("edit.linkReferenceDefinitions")
-      );
+      completionItem.insertText = getDailyNoteLink(date);
+      completionItem.detail = `${completionItem.insertText} - ${detail}`;
       completionItem.command = {
         command: "foam-vscode.open-dated-note",
         title: "Open a note for the given date",
@@ -121,56 +131,29 @@ const completions: CompletionItemProvider = {
 };
 
 const computedCompletions: CompletionItemProvider = {
-  provideCompletionItems: (document, position, token, context) => {
-    return new Promise((resolve, reject) => {
-      // Convert the current position to a range, so that we can see what the user has typed
-      const range = document.getWordRangeAtPosition(position, /\S+/);
-      // Now get what the user has typed. If we don't have a number yet, we still want VS Code to keep "listening"
-      const snippetString = document.getText(range);
-      // Try get a number out of the current snippet string
-      const matches = snippetString.match(/(\d+)/);
-      const number = matches ? matches[0] : undefined;
-      // If we haven't got a number, we return an incomplete list because we want VS Code to keep generating this list
-      if (number === undefined) {
-        return resolve(
-          new CompletionList(
-            [new CompletionItem(snippetString, CompletionItemKind.Snippet)],
-            true
-          )
-        );
-      }
-      // Now if we get here, we have a number we can work with. Lets build a list of possible snippets
-      const completionItems = computedSnippets.map(item => {
-        const { snippet, detail, date } = item(parseInt(number));
-        const completionItem = new CompletionItem(
-          snippet,
-          CompletionItemKind.Snippet
-        );
-        completionItem.range = range;
-        completionItem.insertText = getDailyNoteLink(
-          date,
-          foamConfig.get("openDailyNote.fileExtension"),
-          foamConfig.get("edit.linkReferenceDefinitions")
-        );
-        completionItem.detail = `${completionItem.insertText} - ${detail}`;
-        completionItem.command = {
-          command: "foam-vscode.open-dated-note",
-          title: "Open a note for the given date",
-          arguments: [date]
-        };
-        return completionItem;
-      });
-      // We still want the list to be treated as "incomplete", because the user may add another number
-      return resolve(
-        new CompletionList(
-          [
-            new CompletionItem("/+", CompletionItemKind.Snippet),
-            ...completionItems
-          ],
-          true
-        )
+  provideCompletionItems: (document, position, _token, _context) => {
+    const range = document.getWordRangeAtPosition(position, /\S+/);
+    const snippetString = document.getText(range);
+    const matches = snippetString.match(/(\d+)/);
+    const number: string = matches ? matches[0] : "1";
+    const completionItems = computedSnippets.map(item => {
+      const { snippet, detail, date } = item(parseInt(number));
+      const completionItem = new CompletionItem(
+        snippet,
+        CompletionItemKind.Snippet
       );
+      completionItem.range = range;
+      completionItem.insertText = getDailyNoteLink(date);
+      completionItem.detail = `${completionItem.insertText} - ${detail}`;
+      completionItem.command = {
+        command: "foam-vscode.open-dated-note",
+        title: "Open a note for the given date",
+        arguments: [date]
+      };
+      return completionItem;
     });
+    // We still want the list to be treated as "incomplete", because the user may add another number
+    return new CompletionList(completionItems, true);
   }
 };
 

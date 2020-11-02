47573dc823064a6f0cf813009554145ab19bad48
Riccardo Ferretti <code@riccardoferretti.com>
2020-11-02 21:39:49 +0100

    added tag explorer panel

--- packages/foam-vscode/src/features/index.ts
+++ packages/foam-vscode/src/features/index.ts
@@ -1,12 +1,14 @@
 import createReferences from "./wikilink-reference-generation";
 import openDailyNote from "./open-daily-note";
 import janitor from "./janitor";
-import dataviz from './dataviz'
+import dataviz from "./dataviz";
 import copyWithoutBrackets from "./copy-without-brackets";
 import openDatedNote from "./open-dated-note";
+import tagsExplorer from "./tags-tree-view";
 import { FoamFeature } from "../types";
 
 export const features: FoamFeature[] = [
+  tagsExplorer,
   createReferences,
   openDailyNote,
   janitor,

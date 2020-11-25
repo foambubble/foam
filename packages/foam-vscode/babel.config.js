cdaeefb25243e9c9b743aa004c45977af1eac216
Riccardo Ferretti <code@riccardoferretti.com>
2020-11-25 14:58:10 +0100

    more babel tweaks

--- packages/foam-vscode/babel.config.js
+++ packages/foam-vscode/babel.config.js
@@ -2,5 +2,6 @@ module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-typescript"
-  ]
+  ],
+  plugins: [["@babel/plugin-transform-runtime", { helpers: false }]]
 };

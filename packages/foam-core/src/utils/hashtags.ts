9f17b1f7b9c7f9db372d76576f1ee88f2ccd9a0b
Riccardo <code@riccardoferretti.com>
2020-11-27 13:35:39 +0100

    Fixed parsing of tags (#382)
    * fixed parsing of tags
    * improved regex, courtesy of @jmg-duarte!

--- packages/foam-core/src/utils/hashtags.ts
+++ packages/foam-core/src/utils/hashtags.ts
@@ -1,15 +1,10 @@
 import { isSome, isNumeric } from './core';
-
-const HASHTAG_REGEX = /(^|[ ])#([\w_-]+\b)/gm;
-const WORD_REGEX = /(^|[ ])([\w_-]+\b)/gm;
+const HASHTAG_REGEX = /(^|[ ])#([\w_-]*[a-zA-Z][\w_-]*\b)/gm;
+const WORD_REGEX = /(^|[ ])([\w_-]*[a-zA-Z][\w_-]*\b)/gm;
 
 export const extractHashtags = (text: string): Set<string> => {
   return isSome(text)
-    ? new Set(
-        Array.from([...text.matchAll(HASHTAG_REGEX)])
-          .map(m => m[2].trim())
-          .filter(tag => !isNumeric(tag))
-      )
+    ? new Set(Array.from(text.matchAll(HASHTAG_REGEX), m => m[2].trim()))
     : new Set();
 };
 

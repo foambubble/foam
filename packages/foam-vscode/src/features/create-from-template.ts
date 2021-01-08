3fc37d600b626be0be1d23fbf2bf53bfd74cc53a
Jonathan <jonny@mondago.com>
2021-01-08 14:23:33 +0000

    Provide inline documentation of feature

--- packages/foam-vscode/src/features/create-from-template.ts
+++ packages/foam-vscode/src/features/create-from-template.ts
@@ -11,21 +11,40 @@ import { FoamFeature } from '../types';
 import { TextEncoder } from 'util';
 import { focusNote } from '../utils';
 
-const templatesDir = `${workspace.workspaceFolders[0].uri.path}/.foam/templates`;
+const templatesDir = `${workspace.workspaceFolders[0].uri.fsPath}/.foam/templates`;
+const firstTemplateContent = `# Your new template!
+
+Templates are inserted the same ways snippets are.
+This means you get access to [all these good features!](https://code.visualstudio.com/docs/editor/userdefinedsnippets#_snippet-syntax).
+For example, check out some tabstops in the below list:
+
+- $1
+- $2
+- $3
+
+To create a note from this template, run the 'Foam: Create new note from template' command.
+`;
 
 async function getTemplates(): Promise<string[]> {
   const templates = await workspace.findFiles('.foam/templates/**.md');
   return templates.map(template => path.basename(template.fsPath));
 }
 
+async function offerToCreateTemplate(): Promise<void> {
+  const response = await window.showQuickPick(['Yes', 'No'], {
+    placeHolder:
+      'No templates available. Would you like to create one instead?',
+  });
+  if (response === 'Yes') {
+    commands.executeCommand('foam-vscode.create-new-template');
+    return;
+  }
+}
+
 async function createNoteFromTemplate(): Promise<void> {
   const templates = await getTemplates();
   if (templates.length === 0) {
-    window.showWarningMessage(
-      "No templates found! Creating a template instead..."
-    );
-    commands.executeCommand("foam-vscode.create-new-template");
-    return;
+    return offerToCreateTemplate();
   }
   const activeFile = window.activeTextEditor?.document?.fileName;
   const currentDir =
@@ -66,6 +85,7 @@ async function createNoteFromTemplate(): Promise<void> {
 }
 
 async function createNewTemplate(): Promise<void> {
+  const isFirstTemplate = (await getTemplates()).length === 0;
   const defaultFileName = 'new-template.md';
   const templatesDir = path.join(
     workspace.workspaceFolders[0].uri.fsPath,
@@ -87,11 +107,12 @@ async function createNewTemplate(): Promise<void> {
     return;
   }
 
+  const template = isFirstTemplate ? firstTemplateContent : '';
   await workspace.fs.writeFile(
     Uri.file(filename),
-    new TextEncoder().encode('')
+    new TextEncoder().encode(template)
   );
-  await focusNote(filename, true);
+  await focusNote(filename, false);
 }
 
 const feature: FoamFeature = {

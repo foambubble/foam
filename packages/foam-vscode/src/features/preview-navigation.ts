3329552ef9d8e13eeca9f6a2c07c745a544aa6c2
Riccardo Ferretti <code@riccardoferretti.com>
2021-03-09 17:45:41 +0100

    test: added tests for preview link generation

--- packages/foam-vscode/src/features/preview-navigation.ts
+++ packages/foam-vscode/src/features/preview-navigation.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 import markdownItRegex from 'markdown-it-regex';
-import { Foam, Logger } from 'foam-core';
+import { Foam, FoamWorkspace, Logger } from 'foam-core';
 import { FoamFeature } from '../types';
 
 const feature: FoamFeature = {
@@ -11,39 +11,45 @@ const feature: FoamFeature = {
     const foam = await foamPromise;
 
     return {
-      extendMarkdownIt: (md: markdownit) => {
-        return md.use(markdownItRegex, {
-          name: 'connect-wikilinks',
-          regex: /\[\[([^\[\]]+?)\]\]/,
-          replace: (wikilink: string) => {
-            try {
-              const resource = foam.workspace.find(wikilink);
-              if (resource == null) {
-                return getPlaceholderLink(wikilink);
-              }
-              switch (resource.type) {
-                case 'note':
-                  return `<a class='foam-note-link' title='${resource.title}' href='${resource.uri.fsPath}'>${wikilink}</a>`;
-                case 'attachment':
-                  return `<a class='foam-attachment-link' title='attachment' href='${resource.uri.fsPath}'>${wikilink}</a>`;
-                case 'placeholder':
-                  return getPlaceholderLink(wikilink);
-              }
-            } catch (e) {
-              Logger.error(
-                `Error while creating link for [[${wikilink}]] in Preview panel`,
-                e
-              );
-              return getPlaceholderLink(wikilink);
-            }
-          },
-        });
-      },
+      extendMarkdownIt: (md: markdownit) =>
+        markdownItWithFoamLinks(md, foam.workspace),
     };
   },
 };
 
+export const markdownItWithFoamLinks = (
+  md: markdownit,
+  workspace: FoamWorkspace
+) => {
+  return md.use(markdownItRegex, {
+    name: 'connect-wikilinks',
+    regex: /\[\[([^\[\]]+?)\]\]/,
+    replace: (wikilink: string) => {
+      try {
+        const resource = workspace.find(wikilink);
+        if (resource == null) {
+          return getPlaceholderLink(wikilink);
+        }
+        switch (resource.type) {
+          case 'note':
+            return `<a class='foam-note-link' title='${resource.title}' href='${resource.uri.fsPath}'>${wikilink}</a>`;
+          case 'attachment':
+            return `<a class='foam-attachment-link' title='attachment' href='${resource.uri.fsPath}'>${wikilink}</a>`;
+          case 'placeholder':
+            return getPlaceholderLink(wikilink);
+        }
+      } catch (e) {
+        Logger.error(
+          `Error while creating link for [[${wikilink}]] in Preview panel`,
+          e
+        );
+        return getPlaceholderLink(wikilink);
+      }
+    },
+  });
+};
+
 const getPlaceholderLink = (content: string) =>
-  `<span class='foam-placeholder-link'>${content}</span>`;
+  `<a class='foam-placeholder-link' title="Link to non-existing resource" href="javascript:void(0);">${content}</a>`;
 
 export default feature;

184776a77fb9801475c09e09717f047992d47c60
Riccardo Ferretti <code@riccardoferretti.com>
2021-03-12 19:31:54 +0100

    consolidated "open resource" command code

--- packages/foam-vscode/src/features/utility-commands.ts
+++ packages/foam-vscode/src/features/utility-commands.ts
@@ -7,49 +7,45 @@ import { createNoteFromPlacehoder, focusNote, isSome } from '../utils';
 export const OPEN_COMMAND = {
   command: 'foam-vscode.open-resource',
   title: 'Foam: Open Resource',
+
+  execute: async (params: { resource: vscode.Uri }) => {
+    const { resource } = params;
+    switch (resource.scheme) {
+      case 'file':
+        return vscode.commands.executeCommand('vscode.open', resource);
+
+      case 'placeholder':
+        const newNote = await createNoteFromPlacehoder(resource);
+
+        if (isSome(newNote)) {
+          const title = resource.path.split('/').slice(-1);
+          const snippet = new vscode.SnippetString(
+            '# ${1:' + title + '}\n\n$0'
+          );
+          await focusNote(newNote, true);
+          await vscode.window.activeTextEditor.insertSnippet(snippet);
+        }
+        return;
+
+      case 'attachment':
+        return vscode.window.showInformationMessage(
+          'Opening attachments is not supported yet'
+        );
+    }
+  },
+
   asURI: (resource: vscode.Uri) =>
-    URI.from({
-      scheme: 'command',
-      path: OPEN_COMMAND.command,
-      query: encodeURIComponent(
-        JSON.stringify({
-          resource: resource,
-        })
-      ),
-    }),
+    vscode.Uri.parse(
+      `command:${OPEN_COMMAND.command}?${encodeURIComponent(
+        JSON.stringify({ resource: resource })
+      )}`
+    ),
 };
 
 const feature: FoamFeature = {
   activate: (context: vscode.ExtensionContext) => {
     context.subscriptions.push(
-      commands.registerCommand(
-        OPEN_COMMAND.command,
-        async (params: { resource: vscode.Uri }) => {
-          const { resource } = params;
-          switch (resource.scheme) {
-            case 'file':
-              return vscode.commands.executeCommand('vscode.open', resource);
-
-            case 'placeholder':
-              const newNote = await createNoteFromPlacehoder(resource);
-
-              if (isSome(newNote)) {
-                const title = resource.path.split('/').slice(-1);
-                const snippet = new vscode.SnippetString(
-                  '# ${1:' + title + '}\n\n$0'
-                );
-                await focusNote(newNote, true);
-                await vscode.window.activeTextEditor.insertSnippet(snippet);
-              }
-              return;
-
-            case 'attachment':
-              return vscode.window.showInformationMessage(
-                'Opening attachments is not supported yet'
-              );
-          }
-        }
-      )
+      commands.registerCommand(OPEN_COMMAND.command, OPEN_COMMAND.execute)
     );
   },
 };

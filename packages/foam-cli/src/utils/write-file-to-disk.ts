54a4aec1a04a10b060fe0047977d539812a3486d
Riccardo <code@riccardoferretti.com>
2021-03-18 12:16:40 +0100

    Extracted foam-cli to https://github.com/foambubble/foam-cli (#535)

--- packages/foam-cli/src/utils/write-file-to-disk.ts
+++
@@ -1,6 +0,0 @@
-import * as fs from 'fs';
-import { URI } from 'foam-core';
-
-export const writeFileToDisk = async (fileUri: URI, data: string) => {
-  return fs.promises.writeFile(fileUri.fsPath, data);
-};

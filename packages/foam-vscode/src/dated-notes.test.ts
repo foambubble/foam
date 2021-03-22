dd9826eb351aa24a2c5c0561376b383d7b33e31c
Riccardo Ferretti <code@riccardoferretti.com>
2021-03-22 17:37:27 +0100

    streamlined tests

--- packages/foam-vscode/src/dated-notes.test.ts
+++ packages/foam-vscode/src/dated-notes.test.ts
@@ -1,74 +1,46 @@
-import { workspace } from 'vscode';
+import { Uri, workspace } from 'vscode';
 import { getDailyNotePath } from './dated-notes';
+import path from 'path';
 
 describe('getDailyNotePath', () => {
-  test('Adds the root directory to relative directories (Posix paths)', async () => {
+  test('Adds the root directory to relative directories', async () => {
+    const pathFraments = ['journal', 'subdir'];
     const date = new Date('2021-02-07T00:00:00Z');
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
     const isoDate = `${year}-0${month}-0${day}`;
 
+    const expectedPath = Uri.joinPath(
+      workspace.workspaceFolders[0].uri,
+      ...pathFraments,
+      `${isoDate}.md`
+    );
+
     await workspace
       .getConfiguration('foam')
-      .update('openDailyNote.directory', 'journal/subdir');
+      .update('openDailyNote.directory', path.join(...pathFraments));
     const foamConfiguration = workspace.getConfiguration('foam');
-    expect(getDailyNotePath(foamConfiguration, date).path).toMatch(
-      new RegExp(`journal[\\\\/]subdir[\\\\/]${isoDate}.md$`)
+    expect(getDailyNotePath(foamConfiguration, date).path).toEqual(
+      expectedPath.fsPath
     );
   });
 
-  test('Uses absolute directories without modification (Posix paths)', async () => {
+  test('Uses absolute directories without modification', async () => {
     const date = new Date('2021-02-07T00:00:00Z');
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
     const isoDate = `${year}-0${month}-0${day}`;
+    const config = path.join('absolute_path', 'journal');
+    const expectedPath = path.join(config, `${isoDate}.md`);
 
     await workspace
       .getConfiguration('foam')
-      .update('openDailyNote.directory', '/absolute_path/journal');
+      .update('openDailyNote.directory', config);
     const foamConfiguration = workspace.getConfiguration('foam');
     expect(getDailyNotePath(foamConfiguration, date).path).toMatch(
-      new RegExp(`^/absolute_path/journal/${isoDate}.md`)
-    );
-  });
-
-  test('Adds the root directory to relative directories (Windows paths)', async () => {
-    const date = new Date('2021-02-07T00:00:00Z');
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const isoDate = `${year}-0${month}-0${day}`;
-
-    await workspace
-      .getConfiguration('foam')
-      .update('openDailyNote.directory', 'journal\\subdir');
-    const foamConfiguration = workspace.getConfiguration('foam');
-    expect(getDailyNotePath(foamConfiguration, date).path).toMatch(
-      new RegExp(`journal[\\\\/]subdir[\\\\/]${isoDate}.md$`)
-    );
-  });
-
-  test('Uses absolute directories without modification (Windows paths)', async () => {
-    // While technically the test passes on all OS's, it's only because the test is overly loose.
-    // On Posix systems, this test actually does modify the path, since Windows style paths are
-    // considered to be relative paths. So while this test passes on Posix systems, it is not
-    // because it treats it as an absolute path, but rather that the test doesn't check the same thing.
-    // This was considered "good enough" instead of introducing a dependency like `skip-if` to skip the
-    // test on Posix systems.
-    const date = new Date('2021-02-07T00:00:00Z');
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const isoDate = `${year}-0${month}-0${day}`;
-
-    await workspace
-      .getConfiguration('foam')
-      .update('openDailyNote.directory', 'C:\\absolute_path\\journal');
-    const foamConfiguration = workspace.getConfiguration('foam');
-    expect(getDailyNotePath(foamConfiguration, date).path).toMatch(
-      new RegExp(`/C:[\\\\/]absolute_path[\\\\/]journal[\\\\/]${isoDate}.md`)
+      expectedPath
     );
   });
 });

2858b99818e2a52f23e6e4056b49ed8789e6d065
Riccardo Ferretti <code@riccardoferretti.com>
2021-03-23 19:48:20 +0100

    fixing windows tests

--- packages/foam-vscode/src/dated-notes.test.ts
+++ packages/foam-vscode/src/dated-notes.test.ts
@@ -1,45 +1,48 @@
 import { Uri, workspace } from 'vscode';
 import { getDailyNotePath } from './dated-notes';
-import path from 'path';
+import { URI } from 'foam-core';
+import { isWindows } from './utils';
 
 describe('getDailyNotePath', () => {
+  const date = new Date('2021-02-07T00:00:00Z');
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const isoDate = `${year}-0${month}-0${day}`;
+
   test('Adds the root directory to relative directories', async () => {
-    const pathFraments = ['journal', 'subdir'];
-    const date = new Date('2021-02-07T00:00:00Z');
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const isoDate = `${year}-0${month}-0${day}`;
+    const config = 'journal';
 
     const expectedPath = Uri.joinPath(
       workspace.workspaceFolders[0].uri,
-      ...pathFraments,
+      config,
       `${isoDate}.md`
     );
 
     await workspace
       .getConfiguration('foam')
-      .update('openDailyNote.directory', path.join(...pathFraments));
+      .update('openDailyNote.directory', config);
     const foamConfiguration = workspace.getConfiguration('foam');
-    expect(getDailyNotePath(foamConfiguration, date).path).toEqual(
+
+    expect(URI.toFsPath(getDailyNotePath(foamConfiguration, date))).toEqual(
       expectedPath.fsPath
     );
   });
 
   test('Uses absolute directories without modification', async () => {
-    const date = new Date('2021-02-07T00:00:00Z');
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const isoDate = `${year}-0${month}-0${day}`;
-    const config = path.join('absolute_path', 'journal');
-    const expectedPath = path.join(config, `${isoDate}.md`);
+    const config = isWindows
+      ? 'c:\\absolute_path\\journal'
+      : '/absolute_path/journal';
+    const expectedPath = isWindows
+      ? `${config}\\${isoDate}.md`
+      : `${config}/${isoDate}.md`;
 
     await workspace
       .getConfiguration('foam')
       .update('openDailyNote.directory', config);
     const foamConfiguration = workspace.getConfiguration('foam');
-    expect(getDailyNotePath(foamConfiguration, date).path).toMatch(
+
+    expect(URI.toFsPath(getDailyNotePath(foamConfiguration, date))).toMatch(
       expectedPath
     );
   });

ec7b45c0dd05f0181e7f7e93401a41a31b4d0185
robin <jingjl1001@gmail.com>
2021-04-24 19:24:59 +0800

    chore: Add some description on regex

--- packages/foam-vscode/src/features/link-completion.ts
+++ packages/foam-vscode/src/features/link-completion.ts
@@ -32,7 +32,11 @@ export class CompletionProvider
       .lineAt(position)
       .text.substr(0, position.character);
 
-    // eslint-disable-next-line no-useless-escape
+    /**
+     * Requires autocmplete only if cursorPrefix startting with `[[` and NOT ending with `]]`.
+     * See https://github.com/foambubble/foam/pull/596#issuecomment-825748205 for details.
+     * eslint-disable-next-line no-useless-escape
+     */
     const requiresAutocomplete = cursorPrefix.match(/\[\[[^\[\]]*(?!.*\]\])/);
 
     if (!requiresAutocomplete) {

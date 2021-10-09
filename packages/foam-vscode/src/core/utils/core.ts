f429d829ef9e68b0a7d67222fe137d243e297579
Riccardo Ferretti <code@riccardoferretti.com>
2021-10-09 10:21:28 +0200

    improved linting configuration. core module cannot depend on vscode

--- packages/foam-vscode/src/core/utils/core.ts
+++ packages/foam-vscode/src/core/utils/core.ts
@@ -1,19 +1,19 @@
 import crypto from 'crypto';
 
 export function isNotNull<T>(value: T | null): value is T {
-  return value != null;
+  return value != null; // eslint-disable-line
 }
 
 export function isSome<T>(
   value: T | null | undefined | void
 ): value is NonNullable<T> {
-  return value != null;
+  return value != null; // eslint-disable-line
 }
 
 export function isNone<T>(
   value: T | null | undefined | void
 ): value is null | undefined | void {
-  return value == null;
+  return value == null; // eslint-disable-line
 }
 
 export function isNumeric(value: string): boolean {

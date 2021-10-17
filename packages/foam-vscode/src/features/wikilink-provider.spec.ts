c17c197c405c8ba797263213fa5417d8ea26a50e
Jonas <jojo@srg.com>
2021-10-17 14:55:21 +0200

    WIP init test suite

--- packages/foam-vscode/src/features/wikilink-provider.spec.ts
+++ packages/foam-vscode/src/features/wikilink-provider.spec.ts
@@ -1,5 +1,14 @@
-import { Range } from 'foam-core';
-import { findFirstLineOfContent } from './wikilink-provider';
+import { createMarkdownParser, FoamWorkspace, Range } from 'foam-core';
+import * as vscode from 'vscode';
+import {
+  cleanWorkspace,
+  closeEditors,
+  createFile,
+  createTestWorkspace,
+  showInEditor,
+} from '../test/test-utils';
+import { OPEN_COMMAND } from './utility-commands';
+import { findFirstLineOfContent, WikilinkProvider } from './wikilink-provider';
 
 describe('findFirstLineOfContent', () => {
   it('Should return first line if content start at first line', () => {
@@ -62,4 +71,183 @@ Last line with content
     const content = '\n  \n  \n  \t\n \t\t \n';
     expect(findFirstLineOfContent(content)).toEqual(Range.create(0, 0, 0, 0));
   });
+});
+
+describe('Wikilink Provider', () => {
+  const activate_goToDefinition_forCreation = () => 'goToDefinition';
+  const activate_openLink_forCreation = () => 'openLink';
+  const disableCreationCmd = () => 'off';
+
+  const activateNavigationCmd = () => true;
+  const disableNavigationCmd = () => false;
+
+  const noCancelToken: vscode.CancellationToken = {
+    isCancellationRequested: false,
+    onCancellationRequested: null,
+  };
+
+  const parser = createMarkdownParser([]);
+
+  beforeAll(async () => {
+    await cleanWorkspace();
+  });
+
+  afterAll(async () => {
+    await cleanWorkspace();
+  });
+
+  beforeEach(async () => {
+    await closeEditors();
+  });
+
+  test('for an empty documents should not return any link neither any reference definition', async () => {
+    const { uri, content } = await createFile('');
+    const ws = new FoamWorkspace().set(parser.parse(uri, content));
+
+    const doc = await vscode.workspace.openTextDocument(uri);
+    const provider = new WikilinkProvider(
+      ws,
+      parser,
+      activate_openLink_forCreation,
+      activateNavigationCmd, // link
+      activateNavigationCmd // definition
+    );
+
+    const links = provider.provideDocumentLinks(doc);
+    expect(links.length).toEqual(0);
+
+    const definitions = await provider.provideDefinition(
+      doc,
+      new vscode.Position(0, 0),
+      noCancelToken
+    );
+    expect(definitions).toBeUndefined();
+  });
+
+  // it('should not return any link for documents without links', async () => {
+  //   const { uri, content } = await createFile(
+  //     'This is some content without links'
+  //   );
+  //   const ws = new FoamWorkspace().set(parser.parse(uri, content));
+
+  //   const doc = await vscode.workspace.openTextDocument(uri);
+  //   const provider = new LinkProvider(ws, parser);
+  //   const links = provider.provideDocumentLinks(doc);
+
+  //   expect(links.length).toEqual(0);
+  // });
+
+  test('both providers should support wikilinks', async () => {
+    const fileB = await createFile('# File B');
+    const fileA = await createFile(`this is a link to [[${fileB.name}]].`);
+    const noteA = parser.parse(fileA.uri, fileA.content);
+    const noteB = parser.parse(fileB.uri, fileB.content);
+    const ws = createTestWorkspace()
+      .set(noteA)
+      .set(noteB);
+
+    const { doc } = await showInEditor(noteA.uri);
+    const provider = new WikilinkProvider(
+      ws,
+      parser,
+      activate_openLink_forCreation,
+      activateNavigationCmd, // link
+      activateNavigationCmd // definition
+    );
+
+    const links = provider.provideDocumentLinks(doc);
+    expect(links.length).toEqual(1);
+    expect(links[0].target).toEqual(OPEN_COMMAND.asURI(noteB.uri));
+    expect(links[0].range).toEqual(new vscode.Range(0, 18, 0, 27));
+
+    const definitions = (await provider.provideDefinition(
+      doc,
+      new vscode.Position(0, 20),
+      noCancelToken
+    )) as vscode.LocationLink[];
+    expect(definitions.length).toEqual(1);
+    expect(definitions[0].originSelectionRange).toEqual(
+      new vscode.Range(0, 18, 0, 27)
+    );
+    expect(definitions[0].targetUri.path).toEqual(fileB.uri.path);
+    expect(definitions[0].targetRange).toBeNull();
+    expect(definitions[0].targetSelectionRange).toEqual(
+      new vscode.Range(0, 0, 0, 8)
+    );
+  });
+
+  // it('should support regular links', async () => {
+  //   const fileB = await createFile('# File B');
+  //   const fileA = await createFile(
+  //     `this is a link to [a file](./${fileB.base}).`
+  //   );
+  //   const ws = createTestWorkspace()
+  //     .set(parser.parse(fileA.uri, fileA.content))
+  //     .set(parser.parse(fileB.uri, fileB.content));
+
+  //   const { doc } = await showInEditor(fileA.uri);
+  //   const provider = new LinkProvider(ws, parser);
+  //   const links = provider.provideDocumentLinks(doc);
+
+  //   expect(links.length).toEqual(1);
+  //   expect(links[0].target).toEqual(OPEN_COMMAND.asURI(fileB.uri));
+  //   expect(links[0].range).toEqual(new vscode.Range(0, 18, 0, 38));
+  // });
+
+  // it('should support placeholders', async () => {
+  //   const fileA = await createFile(`this is a link to [[a placeholder]].`);
+  //   const ws = new FoamWorkspace().set(parser.parse(fileA.uri, fileA.content));
+
+  //   const { doc } = await showInEditor(fileA.uri);
+  //   const provider = new LinkProvider(ws, parser);
+  //   const links = provider.provideDocumentLinks(doc);
+
+  //   expect(links.length).toEqual(1);
+  //   expect(links[0].target).toEqual(
+  //     OPEN_COMMAND.asURI(toVsCodeUri(URI.placeholder('a placeholder')))
+  //   );
+  //   expect(links[0].range).toEqual(new vscode.Range(0, 18, 0, 35));
+  // });
+
+  // it('should support wikilinks that have an alias', async () => {
+  //   const fileB = await createFile("# File B that's aliased");
+  //   const fileA = await createFile(
+  //     `this is a link to [[${fileB.name}|alias]].`
+  //   );
+
+  //   const noteA = parser.parse(fileA.uri, fileA.content);
+  //   const noteB = parser.parse(fileB.uri, fileB.content);
+  //   const ws = createTestWorkspace()
+  //     .set(noteA)
+  //     .set(noteB);
+
+  //   const { doc } = await showInEditor(noteA.uri);
+  //   const provider = new LinkProvider(ws, parser);
+  //   const links = provider.provideDocumentLinks(doc);
+
+  //   expect(links.length).toEqual(1);
+  //   expect(links[0].target).toEqual(OPEN_COMMAND.asURI(noteB.uri));
+  //   expect(links[0].range).toEqual(new vscode.Range(0, 18, 0, 33));
+  // });
+
+  // it('should support wikilink aliases in tables using escape character', async () => {
+  //   const fileB = await createFile('# File that has to be aliased');
+  //   const fileA = await createFile(`
+  // | Col A | ColB |
+  // | --- | --- |
+  // | [[${fileB.name}\\|alias]] | test |
+  //   `);
+  //   const noteA = parser.parse(fileA.uri, fileA.content);
+  //   const noteB = parser.parse(fileB.uri, fileB.content);
+  //   const ws = createTestWorkspace()
+  //     .set(noteA)
+  //     .set(noteB);
+
+  //   const { doc } = await showInEditor(noteA.uri);
+  //   const provider = new LinkProvider(ws, parser);
+  //   const links = provider.provideDocumentLinks(doc);
+
+  //   expect(links.length).toEqual(1);
+  //   expect(links[0].target).toEqual(OPEN_COMMAND.asURI(noteB.uri));
+  // });
 });

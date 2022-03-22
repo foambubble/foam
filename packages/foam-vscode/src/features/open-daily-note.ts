6418489c39d47ec559e137bb46240b7f491f653b
Alper Çugun <github@alper.nl>
2022-03-22 23:59:54 +0100

    Initial version to make a note for tomorrow

--- packages/foam-vscode/src/features/open-daily-note.ts
+++ packages/foam-vscode/src/features/open-daily-note.ts
@@ -1,12 +1,25 @@
 import { ExtensionContext, commands } from 'vscode';
 import { FoamFeature } from '../types';
-import { openDailyNoteFor } from '../dated-notes';
 import { getFoamVsCodeConfig } from '../services/config';
+import {
+  openDailyNoteForPickedDate,
+  openDailyNoteForToday,
+} from '../dated-notes';
 
 const feature: FoamFeature = {
   activate: (context: ExtensionContext) => {
     context.subscriptions.push(
-      commands.registerCommand('foam-vscode.open-daily-note', openDailyNoteFor)
+      commands.registerCommand(
+        'foam-vscode.open-daily-note',
+        openDailyNoteForToday
+      )
+    );
+
+    context.subscriptions.push(
+      commands.registerCommand(
+        'foam-vscode.open-daily-note-for-date',
+        openDailyNoteForPickedDate
+      )
     );
 
     if (getFoamVsCodeConfig('openDailyNote.onStartup', false)) {

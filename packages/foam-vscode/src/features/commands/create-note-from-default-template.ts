6c8b94caa3330b792a5d5b543d064b1285a1e3e0
Riccardo Ferretti <code@riccardoferretti.com>
2022-09-30 18:44:33 +0200

    Deprecating create-note-from-default-template command
    It is now replaced by the new and more flexible create-note command

--- packages/foam-vscode/src/features/commands/create-note-from-default-template.ts
+++ packages/foam-vscode/src/features/commands/create-note-from-default-template.ts
@@ -1,14 +1,22 @@
-import { commands, ExtensionContext } from 'vscode';
+import { commands, window, ExtensionContext } from 'vscode';
 import { FoamFeature } from '../../types';
 import { getDefaultTemplateUri, NoteFactory } from '../../services/templates';
 import { Resolver } from '../../services/variable-resolver';
 
+/**
+ * Create a new note from the default template.
+ *
+ * @deprecated use 'foam-vscode.create-note' instead
+ */
 const feature: FoamFeature = {
   activate: (context: ExtensionContext) => {
     context.subscriptions.push(
       commands.registerCommand(
         'foam-vscode.create-note-from-default-template',
         () => {
+          window.showWarningMessage(
+            "This command is deprecated, use 'Foam: Create Note' (foam-vscode.create-note) instead"
+          );
           const resolver = new Resolver(new Map(), new Date());
 
           return NoteFactory.createFromTemplate(

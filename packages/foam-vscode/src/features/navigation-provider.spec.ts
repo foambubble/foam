4251716c727acaea601ad0b757449e6e944f5296
Riccardo Ferretti <code@riccardoferretti.com>
2023-02-12 17:10:18 -0600

    fixed test

--- packages/foam-vscode/src/features/navigation-provider.spec.ts
+++ packages/foam-vscode/src/features/navigation-provider.spec.ts
@@ -13,6 +13,7 @@ import { toVsCodeUri } from '../utils/vsc-utils';
 import { createMarkdownParser } from '../core/services/markdown-parser';
 import { FoamGraph } from '../core/model/graph';
 import { commandAsURI } from '../utils/commands';
+import { CREATE_NOTE_COMMAND } from './commands/create-note';
 
 describe('Document navigation', () => {
   const parser = createMarkdownParser([]);
@@ -83,7 +84,11 @@ describe('Document navigation', () => {
 
       expect(links.length).toEqual(1);
       expect(links[0].target).toEqual(
-        commandAsURI(OPEN_COMMAND.forURI(URI.placeholder('a placeholder')))
+        commandAsURI(
+          CREATE_NOTE_COMMAND.forPlaceholder('a placeholder', {
+            onFileExists: 'open',
+          })
+        )
       );
       expect(links[0].range).toEqual(new vscode.Range(0, 20, 0, 33));
     });

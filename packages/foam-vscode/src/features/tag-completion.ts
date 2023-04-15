1e6614d1665f2161ebaa27eecd875fea66666c96
Jim Graham <james.a.graham+gh@gmail.com>
2023-04-15 12:40:57 -0400

    Addresses #1184
    Currently tag completion only works in the front matter if you type a `#`
    character. Adding the suggested tag will then mark the tag as a comment
    
    ```markdown
    ---
    tags: #foo #bar
    ---
    ```
    
    Update the tag completion provider to recognize if we are in the
    front-matter, by using adding two functions to utils.ts. Because the
    tag completion intellisense must be summoned with either the `#`
    character or the keybinding (typically `ctrl+space`), allow
    for 2 outcomes
    
    1. if the tag is prefixed in the front matter with a `#`, remove it when
       substituting the tag.
    2. If `ctrl-space` is used, recognize we are on the `tags: ` line and
       allow for non-`#` prefaced words.
    
    The tag provider only works on the `tags: ` within the `tags: ` key of
    the frontmatter. For example
    
    ```markdown
    ---
    title: A title
    tags:
     - foo
     - bar
     - |
    ```
    (where `|` is the cursor) will provide suggestions for tags.
    
    Outside the `tags:` element, suggestions will not be provided.
    ```markdown
    ---
    title: A title
    tags:
     - foo
     - bar
    dates:
     - 2023-01-1
     - |
    ```

--- packages/foam-vscode/src/features/tag-completion.ts
+++ packages/foam-vscode/src/features/tag-completion.ts
@@ -2,12 +2,13 @@ import * as vscode from 'vscode';
 import { Foam } from '../core/model/foam';
 import { FoamTags } from '../core/model/tags';
 import { FoamFeature } from '../types';
-import { mdDocSelector } from '../utils';
+import { isInFrontMatter, isOnYAMLKeywordLine, mdDocSelector } from '../utils';
 
 // this regex is different from HASHTAG_REGEX in that it does not look for a
 // #+character. It uses a negative look-ahead for `# `
-const TAG_REGEX =
+const HASH_REGEX =
   /(?<=^|\s)#(?![ \t#])([0-9]*[\p{L}\p{Emoji_Presentation}\p{N}/_-]*)/dgu;
+const FRONT_MATTER_PREVIOUS_CHARACTER = /[#[\s\w]/g;
 
 const feature: FoamFeature = {
   activate: async (
@@ -38,21 +39,46 @@ export class TagCompletionProvider
       .lineAt(position)
       .text.substr(0, position.character);
 
-    const requiresAutocomplete = cursorPrefix.match(TAG_REGEX);
-    if (!requiresAutocomplete) {
+    const beginningOfFileText = document.getText(
+      new vscode.Range(
+        new vscode.Position(0, 0),
+        new vscode.Position(
+          position.line < 50 ? position.line : 50,
+          position.character
+        )
+      )
+    );
+
+    const requiresAutocomplete = cursorPrefix.match(HASH_REGEX);
+    const isHashMatch = requiresAutocomplete !== null;
+    const isTagsMatch =
+      isOnYAMLKeywordLine(beginningOfFileText, 'tags') &&
+      cursorPrefix
+        .charAt(position.character - 1)
+        .match(FRONT_MATTER_PREVIOUS_CHARACTER);
+    const inFrontMatter = isInFrontMatter(beginningOfFileText, position.line);
+
+    if (!isHashMatch && !inFrontMatter) {
+      return null;
+    }
+    if (inFrontMatter && !isTagsMatch) {
       return null;
     }
 
-    // check the match group length.
-    // find the last match group, and ensure the end of that group is
-    // at the cursor position.
-    // This excludes both `#%` and also `here is #my-app1 and now # ` with
-    // trailing space
-    const matches = Array.from(cursorPrefix.matchAll(TAG_REGEX));
-    const lastMatch = matches[matches.length - 1];
-    const lastMatchEndIndex = lastMatch[0].length + lastMatch.index;
-    if (lastMatchEndIndex !== position.character) {
-      return null;
+    let lastMatchStartIndex = -1;
+    if (isHashMatch) {
+      // check the match group length.
+      // find the last match group, and ensure the end of that group is
+      // at the cursor position.
+      // This excludes both `#%` and also `here is #my-app1 and now # ` with
+      // trailing space
+      const matches = Array.from(cursorPrefix.matchAll(HASH_REGEX));
+      const lastMatch = matches[matches.length - 1];
+      lastMatchStartIndex = lastMatch.index;
+      const lastMatchEndIndex = lastMatch[0].length + lastMatchStartIndex;
+      if (lastMatchEndIndex !== position.character) {
+        return null;
+      }
     }
 
     const completionTags = [];
@@ -62,6 +88,19 @@ export class TagCompletionProvider
         vscode.CompletionItemKind.Text
       );
 
+      // if we are in the front matter and we typed #, remove the `#`
+      if (isHashMatch && inFrontMatter) {
+        item.additionalTextEdits = [
+          vscode.TextEdit.delete(
+            new vscode.Range(
+              position.line,
+              lastMatchStartIndex,
+              position.line,
+              lastMatchStartIndex + 1
+            )
+          ),
+        ];
+      }
       item.insertText = `${tag}`;
       item.documentation = tag;
 
--- packages/foam-vscode/src/utils.test.ts
+++ packages/foam-vscode/src/utils.test.ts
@@ -1,4 +1,10 @@
-import { removeBrackets, toTitleCase } from './utils';
+import exp from 'constants';
+import {
+  isInFrontMatter,
+  isOnYAMLKeywordLine,
+  removeBrackets,
+  toTitleCase,
+} from './utils';
 
 describe('removeBrackets', () => {
   it('removes the brackets', () => {
@@ -55,5 +61,68 @@ describe('toTitleCase', () => {
     const actual = toTitleCase(input);
     const expected = 'Word';
     expect(actual).toEqual(expected);
+  });
+});
+
+describe('isInFrontMatter', () => {
+  it('is true for started front matter', () => {
+    const content = `---
+
+`;
+    const actual = isInFrontMatter(content, 1);
+    expect(actual).toBeTruthy();
+  });
+  it('is true for inside completed front matter', () => {
+    const content = '---\ntitle: A title\n---\n';
+    const actual = isInFrontMatter(content, 1);
+    expect(actual).toBeTruthy();
+  });
+  it('is true for inside completed front matter with "..." end delimiter', () => {
+    const content = '---\ntitle: A title\n...\n';
+    const actual = isInFrontMatter(content, 1);
+    expect(actual).toBeTruthy();
+  });
+  it('is false for outside completed front matter', () => {
+    const content = '---\ntitle: A title\n---\ncontent\nmore content\n';
+    const actual = isInFrontMatter(content, 3);
+    expect(actual).toBeFalsy();
+  });
+  it('is false for outside completed front matter with "..." end delimiter', () => {
+    const content = '---\ntitle: A title\n...\ncontent\nmore content\n';
+    const actual = isInFrontMatter(content, 3);
+    expect(actual).toBeFalsy();
+  });
+  it('is false for position on initial front matter delimiter', () => {
+    const content = '---\ntitle: A title\n---\ncontent\nmore content\n';
+    const actual = isInFrontMatter(content, 0);
+    expect(actual).toBeFalsy();
+  });
+  it('is false for position on final front matter delimiter', () => {
+    const content = '---\ntitle: A title\n---\ncontent\nmore content\n';
+    const actual = isInFrontMatter(content, 2);
+    expect(actual).toBeFalsy();
+  });
+
+  describe('isOnYAMLKeywordLine', () => {
+    it('is true if line starts with keyword', () => {
+      const content = 'tags: foo, bar\n';
+      const actual = isOnYAMLKeywordLine(content, 'tags');
+      expect(actual).toBeTruthy();
+    });
+    it('is true if previous line starts with keyword', () => {
+      const content = 'tags: foo\n - bar\n';
+      const actual = isOnYAMLKeywordLine(content, 'tags');
+      expect(actual).toBeTruthy();
+    });
+    it('is false if line starts with wrong keyword', () => {
+      const content = 'tags: foo, bar\n';
+      const actual = isOnYAMLKeywordLine(content, 'title');
+      expect(actual).toBeFalsy();
+    });
+    it('is false if previous line starts with wrong keyword', () => {
+      const content = 'dates:\n - 2023-01-1\n - 2023-01-02\n';
+      const actual = isOnYAMLKeywordLine(content, 'tags');
+      expect(actual).toBeFalsy();
+    });
   });
 });

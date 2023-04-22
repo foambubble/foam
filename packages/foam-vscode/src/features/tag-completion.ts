0f4de170635efed2a862812b9f88ee6aeb8518d1
Jim Graham <james.a.graham+gh@gmail.com>
2023-04-22 09:38:15 -0400

    Refactor into functions for front matter & content
    Refactor the main provider method into two
    sub-functions, one for front matter, one for
    regular content. Add helper functions to generate
    the `CompletionItems` and to find the start & end
    indices of the last match to `#{tag}`.

--- packages/foam-vscode/src/features/tag-completion.ts
+++ packages/foam-vscode/src/features/tag-completion.ts
@@ -8,7 +8,7 @@ import { isInFrontMatter, isOnYAMLKeywordLine, mdDocSelector } from '../utils';
 // #+character. It uses a negative look-ahead for `# `
 const HASH_REGEX =
   /(?<=^|\s)#(?![ \t#])([0-9]*[\p{L}\p{Emoji_Presentation}\p{N}/_-]*)/dgu;
-const FRONT_MATTER_PREVIOUS_CHARACTER = /[#[\s\w]/g;
+const MAX_LINES_FOR_FRONT_MATTER = 50;
 
 const feature: FoamFeature = {
   activate: async (
@@ -43,53 +43,63 @@ export class TagCompletionProvider
       new vscode.Range(
         new vscode.Position(0, 0),
         new vscode.Position(
-          position.line < 50 ? position.line : 50,
+          position.line < MAX_LINES_FOR_FRONT_MATTER
+            ? position.line
+            : MAX_LINES_FOR_FRONT_MATTER,
           position.character
         )
       )
     );
 
-    const requiresAutocomplete = cursorPrefix.match(HASH_REGEX);
-    const isHashMatch = requiresAutocomplete !== null;
-    const isTagsMatch =
-      isOnYAMLKeywordLine(beginningOfFileText, 'tags') &&
-      cursorPrefix
-        .charAt(position.character - 1)
-        .match(FRONT_MATTER_PREVIOUS_CHARACTER);
+    const isHashMatch = cursorPrefix.match(HASH_REGEX) !== null;
     const inFrontMatter = isInFrontMatter(beginningOfFileText, position.line);
 
     if (!isHashMatch && !inFrontMatter) {
       return null;
     }
-    if (inFrontMatter && !isTagsMatch) {
+
+    return inFrontMatter
+      ? this.createTagsForFrontMatter(beginningOfFileText, position)
+      : this.createTagsForContent(cursorPrefix, position);
+  }
+
+  createTagsForFrontMatter(
+    content: string,
+    position: vscode.Position
+  ): vscode.ProviderResult<vscode.CompletionList<vscode.CompletionItem>> {
+    const FRONT_MATTER_PREVIOUS_CHARACTER = /[#[\s\w]/g;
+
+    const lines = content.split('\n');
+    if (position.line >= lines.length) {
       return null;
     }
 
-    let lastMatchStartIndex = -1;
-    if (isHashMatch) {
-      // check the match group length.
-      // find the last match group, and ensure the end of that group is
-      // at the cursor position.
-      // This excludes both `#%` and also `here is #my-app1 and now # ` with
-      // trailing space
-      const matches = Array.from(cursorPrefix.matchAll(HASH_REGEX));
-      const lastMatch = matches[matches.length - 1];
-      lastMatchStartIndex = lastMatch.index;
-      const lastMatchEndIndex = lastMatch[0].length + lastMatchStartIndex;
-      if (lastMatchEndIndex !== position.character) {
-        return null;
-      }
+    const cursorPrefix = lines[position.line].substring(0, position.character);
+
+    const isTagsMatch =
+      isOnYAMLKeywordLine(content, 'tags') &&
+      cursorPrefix
+        .charAt(position.character - 1)
+        .match(FRONT_MATTER_PREVIOUS_CHARACTER);
+
+    if (!isTagsMatch) {
+      return null;
     }
 
-    const completionTags = [];
-    [...this.foamTags.tags].forEach(([tag]) => {
-      const item = new vscode.CompletionItem(
-        tag,
-        vscode.CompletionItemKind.Text
-      );
+    const [lastMatchStartIndex, lastMatchEndIndex] = this.tagMatchIndices(
+      cursorPrefix,
+      HASH_REGEX
+    );
 
-      // if we are in the front matter and we typed #, remove the `#`
-      if (isHashMatch && inFrontMatter) {
+    const isHashMatch = cursorPrefix.match(HASH_REGEX) !== null;
+    if (isHashMatch && lastMatchEndIndex !== position.character) {
+      return null;
+    }
+
+    const completionTags = this.createCompletionTagItems();
+    // We are in the front matter and we typed #, remove the `#`
+    if (isHashMatch) {
+      completionTags.forEach(item => {
         item.additionalTextEdits = [
           vscode.TextEdit.delete(
             new vscode.Range(
@@ -100,14 +110,56 @@ export class TagCompletionProvider
             )
           ),
         ];
-      }
+      });
+    }
+
+    return new vscode.CompletionList(completionTags);
+  }
+
+  createTagsForContent(
+    content: string,
+    position: vscode.Position
+  ): vscode.ProviderResult<vscode.CompletionList<vscode.CompletionItem>> {
+    const [_, lastMatchEndIndex] = this.tagMatchIndices(content, HASH_REGEX);
+    if (lastMatchEndIndex !== position.character) {
+      return null;
+    }
+
+    return new vscode.CompletionList(this.createCompletionTagItems());
+  }
+
+  createCompletionTagItems(): vscode.CompletionItem[] {
+    const completionTags = [];
+    [...this.foamTags.tags].forEach(([tag]) => {
+      const item = new vscode.CompletionItem(
+        tag,
+        vscode.CompletionItemKind.Text
+      );
+
       item.insertText = `${tag}`;
       item.documentation = tag;
 
       completionTags.push(item);
     });
+    return completionTags;
+  }
 
-    return new vscode.CompletionList(completionTags);
+  tagMatchIndices(content: string, match: RegExp): number[] {
+    // check the match group length.
+    // find the last match group, and ensure the end of that group is
+    // at the cursor position.
+    // This excludes both `#%` and also `here is #my-app1 and now # ` with
+    // trailing space
+    const matches = Array.from(content.matchAll(match));
+    if (matches.length === 0) {
+      return [-1, -1];
+    }
+
+    const lastMatch = matches[matches.length - 1];
+    const lastMatchStartIndex = lastMatch.index;
+    const lastMatchEndIndex = lastMatch[0].length + lastMatchStartIndex;
+
+    return [lastMatchStartIndex, lastMatchEndIndex];
   }
 }
 

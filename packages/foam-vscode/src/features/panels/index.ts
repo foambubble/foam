7724c69cc7f406e71312ac3ac8a95708548d2b70
Riccardo Ferretti <code@riccardoferretti.com>
2023-05-02 06:08:36 +0200

    Removed notes-explorer specific code

--- packages/foam-vscode/src/features/panels/index.ts
+++ packages/foam-vscode/src/features/panels/index.ts
@@ -3,4 +3,3 @@ export { default as dataviz } from './dataviz';
 export { default as orphans } from './orphans';
 export { default as placeholders } from './placeholders';
 export { default as tags } from './tags-explorer';
-export { default as notes } from './notes-explorer';

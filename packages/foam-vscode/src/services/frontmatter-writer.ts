af678547474543143be907f5cc087fa3f7b162a5
Karl-Paul <karl-paul2@web.de>
2025-04-25 09:59:00 +0200

    changed FrontmatterWriter parameter from trainNote to any to make Code more adaptable

--- packages/foam-vscode/src/services/frontmatter-writer.ts
+++ packages/foam-vscode/src/services/frontmatter-writer.ts
@@ -1,60 +1,27 @@
 import { IWriter } from '../core/services/Writer/iwriter';
-import { stringify as stringifyYaml } from 'yaml';
-import { TrainNote } from '../core/model/train-note';
 import { URI } from '../core/model/uri';
-import unified from 'unified';
-import visit from 'unist-util-visit';
-import frontmatterPlugin from 'remark-frontmatter';
-import markdownParse from 'remark-parse';
-import markdownStringify from 'remark-stringify';
 import { readFile } from './editor';
+import { workspace } from 'vscode';
+import { toVsCodeUri } from '../utils/vsc-utils';
+import matter from 'gray-matter';
 
 export class FrontmatterWriter implements IWriter {
-  async write(note: TrainNote): Promise<void> {
+  async write(object: any, uri: URI): Promise<void> {
     try {
-      var yaml = this.Transform(note);
-      await this.WriteFrontmatter(yaml, note.uri);
+      const markdown = await readFile(uri);
+      var result = this.ReplaceFrontmatter(markdown, object);
+      await workspace.fs.writeFile(
+        toVsCodeUri(uri),
+        new TextEncoder().encode(result)
+      );
+      console.log(result);
     } catch (error) {
       throw new Error('Could not replace Frontmatter: \n' + error);
     }
   }
 
-  private Transform(note: TrainNote) {
-    const currentPhase = stringifyYaml(note.CurrentPhase());
-    const nextReminder = stringifyYaml(note.nextReminder);
-    var result = currentPhase + nextReminder;
-    if (note.properties != null) {
-      result += stringifyYaml(note.properties);
-    }
-
-    return result;
+  private ReplaceFrontmatter(markdown: string, object: any) {
+    var file = matter(markdown);
+    return matter.stringify(file, object);
   }
-
-  private async WriteFrontmatter(yaml: string, uri: URI) {
-    //unifiedjs anwenden
-    //remarkfrontmatter
-    //custom plugin to replace Frontmatter
-    //remark stringify
-    const markdown = await readFile(uri);
-
-    await unified()
-      .use(markdownParse, { gfm: true })
-      .use(frontmatterPlugin, ['yaml'])
-      .use(replaceFrontmatter, yaml)
-      .use(markdownStringify)
-      .process(markdown);
-
-    console.log(markdown);
-  }
-}
-
-export default function replaceFrontmatter(yaml: string) {
-  //inspiered by: https://github.com/orgs/remarkjs/discussions/1210
-  return function (tree) {
-    visit(tree, node => {
-      if (node.type === 'yaml') {
-        (node as any).value = yaml;
-      }
-    });
-  };
 }

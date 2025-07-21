01aeb55f1cdc69e3b70341523de7a56940ef8698
Riccardo Ferretti <code@riccardoferretti.com>
2025-07-21 15:03:15 +0200

    Use dayjs instead of dateFormat

--- packages/foam-vscode/src/services/js-template-sandbox.ts
+++ packages/foam-vscode/src/services/js-template-sandbox.ts
@@ -1,8 +1,8 @@
 import { TemplateContext } from './note-creation-types';
 import { URI } from '../core/model/uri';
 import { toSlug } from '../utils/slug';
-import dateFormat from 'dateformat';
 import { Logger } from '../core/utils/log';
+import dayjs from 'dayjs';
 
 /**
  * Creates a sandbox environment for JavaScript template execution
@@ -10,18 +10,6 @@ import { Logger } from '../core/utils/log';
  */
 export function createTemplateSandbox(context: TemplateContext) {
   return {
-    // Utility functions
-    dateFormat,
-    slugify: toSlug,
-    URI,
-
-    // Safe console for debugging (logs to Foam output channel)
-    console: {
-      log: (...args: any[]) => Logger.info('[Template]', ...args),
-      warn: (...args: any[]) => Logger.warn('[Template]', ...args),
-      error: (...args: any[]) => Logger.error('[Template]', ...args),
-    },
-
     // Common JavaScript globals (safe subset)
     Date,
     Math,
@@ -34,13 +22,20 @@ export function createTemplateSandbox(context: TemplateContext) {
     RegExp,
     Error,
 
-    // Utility error classes
-    TemplateError: class TemplateError extends Error {
-      constructor(message: string) {
-        super(message);
-        this.name = 'TemplateError';
-      }
+    // Console for debugging (logs to Foam output channel)
+    console: {
+      log: (...args: any[]) =>
+        Logger.info(`[Template] ${args[0]}`, ...args.slice(1)),
+      warn: (...args: any[]) =>
+        Logger.warn(`[Template] ${args[0]}`, ...args.slice(1)),
+      error: (...args: any[]) =>
+        Logger.error(`[Template] ${args[0]}`, ...args.slice(1)),
     },
+
+    // Utility functions
+    dayjs,
+    slugify: toSlug,
+    URI,
   };
 }
 

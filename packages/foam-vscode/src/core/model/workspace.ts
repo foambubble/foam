89298b96526d84f1ff5b4c6ff9e9c46c07ebc88e
Riccardo <code@riccardoferretti.com>
2025-09-25 17:29:42 +0200

    Use identifier case to further disambiguate notes (#1519)
    Fixes #1303

--- packages/foam-vscode/src/core/model/workspace.ts
+++ packages/foam-vscode/src/core/model/workspace.ts
@@ -89,18 +89,26 @@ export class FoamWorkspace implements IDisposable {
 
   public listByIdentifier(identifier: string): Resource[] {
     let needle = this.getTrieIdentifier(identifier);
-
     const mdNeedle =
       getExtension(normalize(identifier)) !== this.defaultExtension
         ? this.getTrieIdentifier(identifier + this.defaultExtension)
         : undefined;
 
-    const resources: Resource[] = [];
+    let resources: Resource[] = [];
 
     this._resources.find(needle).forEach(elm => resources.push(elm[1]));
 
     if (mdNeedle) {
       this._resources.find(mdNeedle).forEach(elm => resources.push(elm[1]));
+    }
+
+    // if multiple resources found, try to filter exact case matches
+    if (resources.length > 1) {
+      resources = resources.filter(
+        r =>
+          r.uri.getBasename() === identifier ||
+          r.uri.getBasename() === identifier + this.defaultExtension
+      );
     }
 
     return resources.sort(Resource.sortByPath);
@@ -115,7 +123,7 @@ export class FoamWorkspace implements IDisposable {
     const amongst = [];
     const basename = forResource.getBasename();
 
-    this.listByIdentifier(basename).map(res => {
+    this.listByIdentifier(basename).forEach(res => {
       // skip self
       if (res.uri.isEqual(forResource)) {
         return;

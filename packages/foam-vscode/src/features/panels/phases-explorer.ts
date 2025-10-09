a834af4a8513e03f0d69c05fdbeda26b47ba3df7
Karl-Paul <karl-paul2@web.de>
2025-10-09 09:36:35 +0200

    feature: added TreeView for Trainnotes and the corresponding phase

---
+++ packages/foam-vscode/src/features/panels/phases-explorer.ts
@@ -0,0 +1,117 @@
+import * as vscode from 'vscode';
+import { FoamWorkspace } from '../../core/model/workspace';
+import {
+  ResourceRangeTreeItem,
+  TrainTreeItem,
+  createBacklinkItemsForResource as createBacklinkTreeItemsForResource,
+} from './utils/tree-view-utils';
+import { FoamGraph } from '../../core/model/graph';
+import {
+  Folder,
+  FolderTreeItem,
+  FolderTreeProvider,
+} from './utils/folder-tree-provider';
+import { TrainNote } from '../../core/model/train-note';
+import { TrainTreeItemBuilder } from './notes-explorer';
+import { Foam } from '../../core/model/foam';
+
+export default async function activate(
+  context: vscode.ExtensionContext,
+  foamPromise: Promise<Foam>
+) {
+  const foam = await foamPromise;
+  const provider = new PhasesProvider(
+    foam.workspace,
+    foam.graph,
+    context.globalState
+  );
+  provider.refresh();
+  const treeView = vscode.window.createTreeView<TrainTreeItems>(
+    'foam-vscode.phases-explorer',
+    {
+      treeDataProvider: provider,
+      showCollapseAll: true,
+      canSelectMany: true,
+    }
+  );
+
+  // const revealTextEditorItem = async () => {
+  //   const target = vscode.window.activeTextEditor?.document.uri;
+  //   if (treeView.visible) {
+  //     if (target) {
+  //       const item = await findTreeItemByUri(provider, target);
+  //       // Check if the item is already selected.
+  //       // This check is needed because always calling reveal() will
+  //       // cause the tree view to take the focus from the item when
+  //       // browsing the notes explorer
+  //       if (
+  //         item &&
+  //         !treeView.selection.find(
+  //           i => i.resourceUri?.path === item.resourceUri.path
+  //         )
+  //       ) {
+  //         treeView.reveal(item);
+  //       }
+  //     }
+  //   }
+  // };
+
+  context.subscriptions.push(
+    treeView,
+    provider,
+    foam.graph.onDidUpdate(() => {
+      provider.refresh();
+    })
+    // vscode.commands.registerCommand(
+    //   `foam-vscode.views.notes-explorer.expand-all`,
+    //   (...args) =>
+    //     expandAll(treeView, provider, node => node.contextValue === 'folder')
+    // ),
+    // vscode.window.onDidChangeActiveTextEditor(revealTextEditorItem),
+    // treeView.onDidChangeVisibility(revealTextEditorItem)
+  );
+}
+
+type TrainTreeItems = TrainTreeItem | ResourceRangeTreeItem;
+export class PhasesProvider extends FolderTreeProvider<
+  TrainTreeItems,
+  TrainNote
+> {
+  constructor(
+    private workspace: FoamWorkspace,
+    private graph: FoamGraph,
+    private state: vscode.Memento
+  ) {
+    super();
+  }
+
+  getValues() {
+    return this.workspace.trainNoteWorkspace.list();
+  }
+
+  getFilterFn() {
+    return () => true;
+  }
+
+  valueToPath(value: TrainNote) {
+    return [value.currentPhase.name, value.title];
+  }
+
+  createValueTreeItem(
+    value: TrainNote,
+    parent: FolderTreeItem<TrainNote>,
+    node: Folder<TrainNote>
+  ) {
+    return new TrainTreeItemBuilder(value, this.workspace)
+      .setDescription()
+      .setWorkspace(this.workspace)
+      .setOptions(
+        parent,
+        this.graph.getBacklinks(value.uri).length > 0
+          ? vscode.TreeItemCollapsibleState.Collapsed
+          : vscode.TreeItemCollapsibleState.None
+      )
+      .setGraph(this.graph)
+      .build();
+  }
+}

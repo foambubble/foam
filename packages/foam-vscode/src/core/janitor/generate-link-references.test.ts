efbc6e6fd532fd28d6f2baa61067e9a9bde5d516
Riccardo Ferretti <code@riccardoferretti.com>
2025-12-13 18:00:56 +0100

    Changed replacement approach. Changed tests to check results.

--- packages/foam-vscode/src/core/janitor/generate-link-references.test.ts
+++ packages/foam-vscode/src/core/janitor/generate-link-references.test.ts
@@ -5,19 +5,30 @@ import { Logger } from '../utils/log';
 import { URI } from '../model/uri';
 import { EOL } from 'os';
 import { createMarkdownParser } from '../services/markdown-parser';
+import { TextEdit } from '../services/text-edit';
 
 Logger.setLevel('error');
 
+/**
+ * Will adjust a text line separator to match
+ * what is used by the note
+ * Necessary when running tests on windows
+ *
+ * @param note the note we are adjusting for
+ * @param text starting text, using a \n line separator
+ */
+function textForNote(text: string): string {
+  const eol = EOL;
+  return text.split('\n').join(eol);
+}
+
 describe('generateLinkReferences', () => {
+  const parser = createMarkdownParser();
+
   it('should add link references to a file that does not have them', async () => {
-    const parser = createMarkdownParser();
     const workspace = createTestWorkspace([URI.file('/')]);
-    workspace.set(
-      createTestNote({ uri: '/first-document.md', title: 'First Document' })
-    );
-    workspace.set(
-      createTestNote({ uri: '/second-document.md', title: 'Second Document' })
-    );
+    workspace.set(createTestNote({ uri: '/doc1.md', title: 'First' }));
+    workspace.set(createTestNote({ uri: '/doc2.md', title: 'Second' }));
     workspace.set(
       createTestNote({
         uri: '/file-without-title.md',
@@ -25,64 +36,10 @@ describe('generateLinkReferences', () => {
       })
     );
     const noteText = `# Index
-
-This file is intentionally missing the link reference definitions
-
-[[first-document]]
-
-[[second-document]]
-
-[[file-without-title]]
+[[doc1]] [[doc2]] [[file-without-title]]
 `;
 
     const note = parser.parse(URI.file('/note.md'), textForNote(noteText));
-
-    const expected = {
-      newText: textForNote(
-        `
-[first-document]: first-document "First Document"
-[second-document]: second-document "Second Document"
-[file-without-title]: file-without-title "file-without-title"`
-      ),
-      range: Range.create(9, 0, 9, 0),
-    };
-
-    const noteEol = EOL;
-    const actual = await generateLinkReferences(
-      note,
-      noteText,
-      noteEol,
-      workspace,
-      false
-    );
-
-    expect(actual![0].range.start).toEqual(expected.range.start);
-    expect(actual![0].range.end).toEqual(expected.range.end);
-    expect(actual![0].newText).toEqual(expected.newText);
-  });
-
-  it('should remove link definitions from a file that has them, if no links are present', async () => {
-    const parser = createMarkdownParser();
-    const workspace = createTestWorkspace([URI.file('/')]);
-    workspace.set(
-      createTestNote({ uri: '/first-document.md', title: 'First Document' })
-    );
-    const noteText = `# Second Document
-
-This is just a link target for now.
-
-We can use it for other things later if needed.
-
-[first-document]: first-document 'First Document'
-`;
-
-    const note = parser.parse(URI.file('/note.md'), textForNote(noteText));
-
-    const expected = {
-      newText: '',
-      range: Range.create(6, 0, 6, 49),
-    };
-
     const actual = await generateLinkReferences(
       note,
       noteText,
@@ -90,19 +47,86 @@ We can use it for other things later if needed.
       workspace,
       false
     );
+    const updated = TextEdit.apply(noteText, actual);
 
-    expect(actual.length).toBe(1);
-    expect(actual[0]!.range.start).toEqual(expected.range.start);
-    expect(actual[0]!.range.end).toEqual(expected.range.end);
-    expect(actual[0]!.newText).toEqual(expected.newText);
+    expect(updated).toBe(
+      textForNote(`# Index
+[[doc1]] [[doc2]] [[file-without-title]]
+
+[doc1]: doc1 "First"
+[doc2]: doc2 "Second"
+[file-without-title]: file-without-title "file-without-title"
+`)
+    );
   });
 
-  it('should update link definitions if they are present but changed', async () => {
-    const parser = createMarkdownParser();
+  it('#1558 - should add multiple link references without excessive blank lines', async () => {
     const workspace = createTestWorkspace([URI.file('/')]);
-    workspace.set(
-      createTestNote({ uri: '/second-document.md', title: 'Second Document' })
+    workspace.set(createTestNote({ uri: '/Doc1.md', title: 'Doc1' }));
+    workspace.set(createTestNote({ uri: '/Doc2.md', title: 'Doc2' }));
+
+    const noteText = `# Test
+
+[[Doc1]]
+
+[[Doc2]]
+
+
+
+`;
+
+    const note = parser.parse(URI.file('/note.md'), textForNote(noteText));
+    const actual = await generateLinkReferences(
+      note,
+      noteText,
+      EOL,
+      workspace,
+      false
     );
+    const updated = TextEdit.apply(noteText, actual);
+
+    expect(updated).toBe(
+      textForNote(`# Test
+
+[[Doc1]]
+
+[[Doc2]]
+
+[Doc1]: Doc1 "Doc1"
+[Doc2]: Doc2 "Doc2"
+`)
+    );
+  });
+
+  it('should remove link definitions for links not present in note', async () => {
+    const workspace = createTestWorkspace([URI.file('/')]);
+    workspace.set(createTestNote({ uri: '/doc1.md', title: 'First' }));
+    const noteText = `# Document
+Some content here.
+[doc1]: doc1 'First Document'
+`;
+
+    const note = parser.parse(URI.file('/note.md'), textForNote(noteText));
+    const actual = await generateLinkReferences(
+      note,
+      noteText,
+      EOL,
+      workspace,
+      false
+    );
+    const updated = TextEdit.apply(noteText, actual);
+
+    expect(updated).toBe(
+      textForNote(`# Document
+Some content here.
+
+`)
+    );
+  });
+
+  it('should add missing link definitions', async () => {
+    const workspace = createTestWorkspace([URI.file('/')]);
+    workspace.set(createTestNote({ uri: '/doc2.md', title: 'Second' }));
     workspace.set(
       createTestNote({
         uri: '/file-without-title.md',
@@ -117,24 +141,10 @@ Here's some [unrelated] content.
 
 [[file-without-title]]
 
-[second-document]: second-document 'Second Document'
+[doc2]: doc2 'Second Document'
 `;
 
     const note = parser.parse(URI.file('/note.md'), textForNote(noteText));
-
-    const expected = [
-      {
-        newText: '',
-        range: Range.create(8, 0, 8, 52),
-      },
-      {
-        newText: textForNote(
-          `\n[file-without-title]: file-without-title "file-without-title"`
-        ),
-        range: Range.create(9, 0, 9, 0),
-      },
-    ];
-
     const actual = await generateLinkReferences(
       note,
       noteText,
@@ -142,55 +152,54 @@ Here's some [unrelated] content.
       workspace,
       false
     );
+    const updated = TextEdit.apply(noteText, actual);
 
-    expect(actual.length).toBe(2);
-    expect(actual[0]!.range.start).toEqual(expected[0].range.start);
-    expect(actual[0]!.range.end).toEqual(expected[0].range.end);
-    expect(actual[0]!.newText).toEqual(expected[0].newText);
-    expect(actual[1]!.range.start).toEqual(expected[1].range.start);
-    expect(actual[1]!.range.end).toEqual(expected[1].range.end);
-    expect(actual[1]!.newText).toEqual(expected[1].newText);
+    expect(updated).toBe(
+      textForNote(`# First Document
+
+Here's some [unrelated] content.
+
+[unrelated]: http://unrelated.com 'This link should not be changed'
+
+[[file-without-title]]
+
+
+
+[file-without-title]: file-without-title "file-without-title"
+`)
+    );
   });
 
   it('should not cause any changes if link reference definitions were up to date', async () => {
-    const parser = createMarkdownParser();
     const workspace = createTestWorkspace([URI.file('/')])
-      .set(
-        createTestNote({ uri: '/first-document.md', title: 'First Document' })
-      )
-      .set(
-        createTestNote({ uri: '/second-document.md', title: 'Second Document' })
-      );
+      .set(createTestNote({ uri: '/doc1.md', title: 'First' }))
+      .set(createTestNote({ uri: '/doc2.md', title: 'Second' }));
     const noteText = `# Third Document
-
 All the link references are correct in this file.
 
-[[first-document]]
+[[doc1]]
+[[doc2]]
 
-[[second-document]]
 
-[first-document]: first-document "First Document"
-[second-document]: second-document "Second Document"
+[doc1]: doc1 "First"
+[doc2]: doc2 "Second"
 `;
 
     const note = parser.parse(URI.file('/note.md'), textForNote(noteText));
-
-    const expected = [];
-
-    const noteEol = EOL;
     const actual = await generateLinkReferences(
       note,
       noteText,
-      noteEol,
+      EOL,
       workspace,
       false
     );
+    const updated = TextEdit.apply(noteText, actual);
 
-    expect(actual).toEqual(expected);
+    expect(actual).toEqual([]);
+    expect(updated).toEqual(noteText);
   });
 
   it('should put links with spaces in angel brackets', async () => {
-    const parser = createMarkdownParser();
     const workspace = createTestWorkspace([URI.file('/')]).set(
       createTestNote({
         uri: '/Note being referred as angel.md',
@@ -202,15 +211,6 @@ All the link references are correct in this file.
 [[Note being referred as angel]]
 `;
     const note = parser.parse(URI.file('/note.md'), textForNote(noteText));
-
-    const expected = {
-      newText: textForNote(
-        `
-[Note being referred as angel]: <Note being referred as angel> "Note being referred as angel"`
-      ),
-      range: Range.create(3, 0, 3, 0),
-    };
-
     const actual = await generateLinkReferences(
       note,
       noteText,
@@ -218,19 +218,21 @@ All the link references are correct in this file.
       workspace,
       false
     );
+    const updated = TextEdit.apply(noteText, actual);
 
-    expect(actual.length).toBe(1);
-    expect(actual[0]!.range.start).toEqual(expected.range.start);
-    expect(actual[0]!.range.end).toEqual(expected.range.end);
-    expect(actual[0]!.newText).toEqual(expected.newText);
+    expect(updated).toBe(
+      textForNote(`# Angel reference
+
+[[Note being referred as angel]]
+
+[Note being referred as angel]: <Note being referred as angel> "Note being referred as angel"
+`)
+    );
   });
 
   it('should not remove explicitly entered link references', async () => {
-    const parser = createMarkdownParser();
     const workspace = createTestWorkspace([URI.file('/')]);
-    workspace.set(
-      createTestNote({ uri: '/second-document.md', title: 'Second Document' })
-    );
+    workspace.set(createTestNote({ uri: '/doc2.md', title: 'Second' }));
     workspace.set(
       createTestNote({
         uri: '/file-without-title.md',
@@ -248,8 +250,6 @@ A Bug [^footerlink]. Here is [Another link][linkreference]
 
     const note = parser.parse(URI.file('/note.md'), textForNote(noteText));
 
-    const expected = [];
-
     const actual = await generateLinkReferences(
       note,
       noteText,
@@ -258,24 +258,21 @@ A Bug [^footerlink]. Here is [Another link][linkreference]
       false
     );
 
-    expect(actual).toEqual(expected);
+    expect(actual).toEqual([]);
   });
 
   it('should not remove explicitly entered link references and have an implicit link', async () => {
-    const parser = createMarkdownParser();
     const workspace = createTestWorkspace([URI.file('/')]);
-    workspace.set(
-      createTestNote({ uri: '/second-document.md', title: 'Second Document' })
-    );
+    workspace.set(createTestNote({ uri: '/doc2.md', title: 'Second' }));
     const noteText = `# File with explicit link references
 
 A Bug [^footerlink]. Here is [Another link][linkreference].
-I also want a [[first-document]].
+I also want a [[doc1]].
 
 [^footerlink]: https://foambubble.github.io/
 
 [linkreference]: https://foambubble.github.io/
-[first-document]: first-document 'First Document'
+[doc1]: doc1 'First Document'
 `;
 
     const note = parser.parse(URI.file('/note.md'), textForNote(noteText));
@@ -291,16 +288,3 @@ I also want a [[first-document]].
     expect(actual.length).toBe(0);
   });
 });
-
-/**
- * Will adjust a text line separator to match
- * what is used by the note
- * Necessary when running tests on windows
- *
- * @param note the note we are adjusting for
- * @param text starting text, using a \n line separator
- */
-function textForNote(text: string): string {
-  const eol = EOL;
-  return text.split('\n').join(eol);
-}

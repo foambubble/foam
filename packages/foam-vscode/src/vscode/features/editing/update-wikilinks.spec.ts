95cb1aec5677b739ddb7dc28a9b916d335d35bd7
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2026-07-19 09:17:32 +0000

    Remove test for code not in branch

--- packages/foam-vscode/src/vscode/features/editing/update-wikilinks.spec.ts
+++ packages/foam-vscode/src/vscode/features/editing/update-wikilinks.spec.ts
@@ -54,41 +54,4 @@ describe('Update wikilink definitions', () => {
 [doc3]: doc3 "Third"
 `);
   });
-
-  it('orders existing definitions by wikilink occurrence when configured', async () => {
-    const doc1 = await createFile('# First', ['doc1.md']);
-    const doc2 = await createFile('# Second', ['doc2.md']);
-    const source = await createFile(`[[doc2]] [[doc1]]
-
-[doc1]: doc1 "First"
-[doc2]: doc2 "Second"
-`);
-
-    await waitForNoteInFoamWorkspace(doc1.uri);
-    await waitForNoteInFoamWorkspace(doc2.uri);
-    await waitForNoteInFoamWorkspace(source.uri);
-    const { editor } = await showInEditor(source.uri);
-
-    await withModifiedFoamConfiguration(
-      'edit.linkReferenceDefinitions',
-      'withoutExtensions',
-      async () => {
-        await withModifiedFoamConfiguration(
-          'edit.linkReferenceDefinitionsSort',
-          'occurrence',
-          async () => {
-            await vscode.commands.executeCommand(
-              'foam-vscode.update-wikilink-definitions'
-            );
-          }
-        );
-      }
-    );
-
-    expect(editor.document.getText()).toBe(`[[doc2]] [[doc1]]
-
-[doc2]: doc2 "Second"
-[doc1]: doc1 "First"
-`);
-  });
 });
